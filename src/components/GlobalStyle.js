36ac6ff620828e06751c5c51fcbdd8fe1851d0b1
kangansoo <ansoo971@gamail.com>
2024-01-02 16:47:55 +0900

    :lipstick: 폰트 파일 생성 및 적용

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle} from "styled-components";
+import "../css/font.css"
+
+const GlobalStyle = createGlobalStyle`
+body {
+    font-family: "KNUTRUTH";
+    text-color: black;
+}
+`
+
+export default GlobalStyle;
