fa6a0ae7af65b8610a3f8640aae20550122fc4bc
kangansoo <ansoo971@gamail.com>
2024-01-03 16:29:26 +0900

    :memo: Projects 컴포넌트, .env 생성

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/Projects.css'
+
+export default function Projects() {
+  return (
+    <div className="Projects">
+      <div>
+        
+      </div>
+    </div>
+  )
+}
