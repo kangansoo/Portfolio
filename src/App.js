b07ce49df0be467c468ce10d980f6c989c8f9ff0
kangansoo <ansoo971@gamail.com>
2024-01-03 14:12:30 +0900

    :lipstick: About 페이지 틀 및 스킬 페이지 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import About from './components/About';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className='AppWrapper'>
       <Header />
       <About />
+      <Skills />
       <Footer />
     </div>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import '../css/About.css'
+import React from "react";
+import "../css/About.css";
 
 export default function About() {
   return (
-    <div className='About'>
-        <div className='Text'>
-            <h2>레이아웃</h2>
+    <div className="AboutBG">
+      <div className="StarContainer">
+        <div className="star1"></div>
+        <div className="star2"></div>
+        <div className="star3"></div>
+      </div>
+      <div className="About">
+        <div className="AboutContainer">
+          <div className="Memoji"></div>
+          <div className="ProfileContainer">
+            <div className="Name">
+              강안수
+            </div>
+            <div className="Birth">
+              1997.01.24
+            </div>
+            <div className="University">
+              중국인민대학교
+            </div>
+            <div className="Number">
+              +82-10-3622-8749
+            </div>
+            <div className="Email">
+              ansoo971@gmail.com
+            </div>
+          </div>  
         </div>
+        <div className='AboutText'>
+          안녕하세요! IT 서비스 기획자가 최종 목표인 프론트엔드 개발자입니다.
+        </div>
+      </div>
     </div>
-  )
+  );
 }
