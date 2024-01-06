072e29abf512191f4648f873a3fdfbf017421061
kangansoo <ansoo971@gamail.com>
2024-01-06 12:30:06 +0900

    :lipstick: About 작성

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "../css/About.css";
+import user from '../assets/user.png';
+import bachelor from '../assets/bachelor.png';
+import birth from '../assets/birth.png';
+import home from '../assets/home.png';
+import phone from '../assets/phone.png';
+import email from '../assets/email.png';
 
 export default function About() {
   return (
@@ -16,25 +22,54 @@ export default function About() {
         <div className="AboutContainer">
           <div className="Memoji"></div>
           <div className="ProfileContainer">
-            <div className="Name">
-              강안수
+            <div className="ProfilSmallContainer">
+              <img src={user} alt="user" className="ProfileIcon" />
+              <div className="Profile">
+                강안수
+              </div>
             </div>
-            <div className="Birth">
-              1997.01.24
+            <div className="ProfilSmallContainer">
+              <img src={birth} alt="birth" className="ProfileIcon" />
+              <div className="Profile">
+                1997.01.24
+              </div>
             </div>
-            <div className="University">
-              중국인민대학교
+            <div className="ProfilSmallContainer">
+              <img src={phone} alt="phone" className="ProfileIcon" />
+              <div className="Profile">
+                +82-10-3622-8749
+              </div>
             </div>
-            <div className="Number">
-              +82-10-3622-8749
+            <div className="ProfilSmallContainer">
+              <img src={home} alt="home" className="ProfileIcon" />
+              <div className="Profile">
+                서울시 강서구
+              </div>
             </div>
-            <div className="Email">
-              ansoo971@gmail.com
+            <div className="ProfilSmallContainer">
+              <img src={bachelor} alt="bachelor" className="ProfileIcon" />
+              <div className="Profile">
+                중국인민대학교(경제학)
+              </div>
+            </div>
+            <div className="ProfilSmallContainer">
+              <img src={email} alt="email" className="ProfileIcon" />
+              <div className="Profile">
+                ansoo971@gmail.com
+              </div>
             </div>
           </div>  
         </div>
-        <div className='AboutText'>
-          안녕하세요! IT 서비스 기획자가 최종 목표인 프론트엔드 개발자입니다.
+        <div className='AboutTextContainer'>
+          <p className='aboutText'>
+            안녕하세요! IT 서비스 기획자가 최종 목표인 프론트엔드 개발자입니다.
+          </p>
+          <p className='aboutText'>
+            다름을 인정하고 시야를 넓히기 위해 배우는 자세로 소통하며,
+          </p>
+          <p className='aboutText'>
+            성장하기 위해 노력하고 있습니다.
+          </p>
         </div>
       </div>
     </div>
