ddf2ecfec4f2a238138bca5ea49381487941177a
kangansoo <ansoo971@gamail.com>
2024-01-06 16:54:28 +0900

    :lipstick: Footer에서 git, notion 뱃지 삭제

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,7 @@ import '../css/Footer.css'
 export default function Footer() {
   return (
     <div className="Footer">
-        <div className="BadgesContainer">
-          <div className="BadgeContainer">
-            <img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white" alt="" />
-          </div>
-          <div className="BadgeContainer">
-            <img src="https://img.shields.io/badge/notion-000000?style=for-the-badge&logo=notion&logoColor=white" alt="" /> 
-          </div>
-        </div>
+      
     </div>
   )
 }
