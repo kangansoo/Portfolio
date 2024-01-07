20ea816e664e37122085beb64cc75bc674c8fc58
kangansoo <ansoo971@gamail.com>
2024-01-07 14:03:44 +0900

    :lipstick: Project 완성 및 skills 이미지 추가

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,9 @@ const Projects = forwardRef((props, ref) => {
       <div className="ProjectsContainer">
         <div className="ProjectContainer">
           <div className="Project">
-            <div className="ProjectTitle">천안시 데이터 시각화 아이디어 공모전</div>
+            <a href="https://kangansoo.notion.site/169ba524ce6a434d9410894291a57045" target="_blank" rel="noreferrer">
+              <div className="ProjectTitle">천안시 데이터 시각화 아이디어 공모전</div>
+            </a>
             <div className="ProjectDetail">
               <p className="DetailTitle">• Tools :</p>
               <div className="ToolsContainer">
@@ -33,7 +35,9 @@ const Projects = forwardRef((props, ref) => {
                   Scikit-Learn의 선형 회귀 모델을 사용하여 적정 공공자전거 대여소 수를 예측하였습니다.
                   또한, plotly를 사용하여 실제 천안시 지도에 공공자전거 대여소의 좌표를 표시하였고, Tableu를 통해 EDA 및 
                   데이터 분석 과정을 시각화하였습니다.<br />
-                  첫 프로젝트를 경험하며 Notion, Git Hub 등의 협업 및 소통 방식에 대해 배울 수 있었습니다. 
+                  첫 프로젝트를 경험하며 Notion, Git Hub 등의 협업 및 소통 방식에 대해 배울 수 있었고 
+                  부트캠프에서 배운 내용을 복습하였고, 첫 공모전에서 비록 수상은 아니지만
+                  실제 PT까지 준비하며 성취감을 느낄 수 있었습니다.
                 </p>
               </div>
             </div>
@@ -41,7 +45,9 @@ const Projects = forwardRef((props, ref) => {
         </div>
         <div className="ProjectContainer">
           <div className="Project">
-            <div className="ProjectTitle">개인화 맞춤형 VOD 추천 서비스</div>
+            <a href="https://kangansoo.notion.site/VOD-adf62df4460a47a7a3374b49ca087271" target="_blank" rel="noreferrer">
+              <div className="ProjectTitle">개인화 맞춤형 VOD 추천 서비스</div>
+            </a>
             <div className="ProjectDetail">
               <p className="DetailTitle">• Tools :</p>
               <div className="ToolsContainer">
@@ -74,7 +80,11 @@ const Projects = forwardRef((props, ref) => {
                 <p className="DetailText">
                   LG 헬로비전 DX DATA School의 최종 프로젝트로서, LG 헬로비전의 실제 로그 데이터를 활용해
                   개인화 맞춤 VOD 추천을 하는 서비스입니다. <br />
-                  저는 프론트엔드를 담당하였고, React로 웹 페이지를 구현하였습니다. 
+                  저는 프론트엔드를 담당하였고, React로 웹 페이지를 구현하였습니다. 프로젝트 시작 전 피그마를 활용하여 웹 페이지를 기획하였고
+                  css와 styledcomponents를 같이 이용하여 css 중복 생성을 최소화하였습니다.
+                  리액트 훅들을 적극 활용하여 찜, 평점, 리뷰와 같은 기능들을 구현하였습니다. 기능들, 예를 들어 캐러셀, 모달 등을 구현할 때에도 다양한 라이브러리들을
+                  적용하며 비교해 다양한 라이브러리들을 경험할 수 있었습니다. 서버에서 받아오는 데이터들의 상태를 리덕스로 관리하여 로딩페이지를 구현하였습니다.
+                  Git과 AWS를 통해 CI/CD를 구현하였고 슬랙을 통해 팀간 소통을 하였고 노션을 통해 회의, 의견 등의 문서화를 하였습니다.
                 </p>
               </div>
             </div>
