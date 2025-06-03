267f4cd7cac097af2be5768c8e9bb63244230f27
kangansoo <ansoo971@gmail.com>
2025-06-03 17:16:22 +0900

    :tada: init: 프로젝트 생성

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pretendard: ["Pretendard"],
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
