f25310d39ce0f79636a210f4551e636b3fdd6108
kangansoo <ansoo971@gmail.com>
2025-09-14 14:17:15 +0900

    :lipstick: Skills 초안 완료

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LandingComponent from "@/components/LandingComponent"
 import Header from "@/components/Header"
 import Hero from "@/components/Hero"
 import Projects from "@/components/Projects"
+import Skills from "@/components/Skills"
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           </div>
           <div className="w-full h-2/3 shrink-0">
             <Projects />
+          </div>
+          <div className="w-full h-1/2 shrink-0">
+            <Skills />
           </div>
         </div>
       </div>
---
+++ src/components/Skills.tsx
@@ -0,0 +1,19 @@
+const Skills = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center select-none">
+      <div className="w-[60%] h-[80%]">
+        <p className="font-nanumsquare text-2xl font-bold text-font-color">Skills</p>
+        <div className="w-full flex flex-col font-nexon text-font-color/80">
+          <p className="text-lg mt-5 mb-2">strong</p>
+          <p className="text-sm">JavaScript / React.js / Next.js / Redux / React-Query / TypeScript / HTML5 / CSS / Tailwindcss / Styled Components / Java / WebSocket / WebRTC</p>
+          <p className="text-lg mt-5 mb-2">experienced</p>
+          <p className="text-sm">Vue.js / Spring / Python / scikit-learn / Numpy / Pandas / MySQL </p>
+          <p className="text-lg mt-5 mb-2">etc</p>
+          <p className="text-sm">Figma / Git / Jira</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
