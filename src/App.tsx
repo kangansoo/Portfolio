67753a37accc6ad35f13ad2ec27f9019c8d42260
kangansoo <ansoo971@gmail.com>
2025-09-14 14:55:18 +0900

    :memo: react-icons 라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import Hero from "@/components/Hero"
 import Projects from "@/components/Projects"
 import Skills from "@/components/Skills"
 import Exprience from "./components/Exprience"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="w-full h-screen">
       <LandingComponent />
       <div className="w-full h-full bg-main-bg">
-        <div className="h-12 top-0 sticky flex bg-main-bg z-999">
+        <div className="h-12 top-0 sticky flex bg-main-bg z-40">
           <Header />
         </div>
         <div className="w-full h-full flex flex-col">
@@ -26,6 +27,9 @@ function App() {
           </div>
           <div className="w-full h-1/4 shrink-0">
             <Exprience />
+          </div>
+          <div className="w-full h-12 bg-gray-100">
+            <Footer />
           </div>
         </div>
       </div>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full h-full flex flex-row justify-center items-center">
+      <div className="w-[60%] h-[80%] flex flex-row justify-start items-center my-5">
+        <div>z</div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
