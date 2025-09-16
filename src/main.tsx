74ccdf959154273cc9eca3e0e13807d8fc98b343
kangansoo <ansoo971@gmail.com>
2025-09-16 16:18:11 +0900

    :sparkles: 페이지 이동 시 스크롤 최상단 유지

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-import App from "./App.tsx"
-import "./index.css"
+import App from "@/App.tsx"
+import "@/index.css"
+import ScrollToTop from "@/components/ScrollToTop.ts"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
