c1947d238e914e1b02659ab0d5443a50f1ae651c
kangansoo <ansoo971@gmail.com>
2025-10-11 22:19:42 +0900

    :memo: 소개글 수정

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,8 +7,8 @@ const About = () => {
         <p className="font-nanumsquare text-2xl font-bold text-font-color mt-10">About</p>
         <div className="w-full flex flex-col font-nexon text-font-color/80 text-sm mt-5">
           <p>
-            좋은 서비스란 사용자가 <Strong fontSize="md">믿고 쓸 수 있는</Strong> 서비스라고 생각합니다. 이러한 신뢰는 언제나 안정적으로 작동하는 <Strong fontSize="md">안정성</Strong>, 직관적이고
-            편리한 <Strong fontSize="md">사용성</Strong>, 그리고 사용자의 니즈를 정확히 반영하는 <Strong fontSize="md">편의성</Strong>에서 나온다고 믿습니다. 저는 이 세 가지 원칙을 개발 철칙으로
+            좋은 서비스란 사용자가 <Strong fontSize="md">믿고 쓸 수 있는</Strong> 서비스라고 생각합니다. 이러한 믿음은 언제나 일관적되게 작동하는 <Strong fontSize="md">안정성</Strong>, 직관적이고
+            편리한 <Strong fontSize="md">사용성</Strong> 그리고 사용자에게 최적의 경험을 제공하는 <Strong fontSize="md">편의성</Strong>에서 나온다고 믿습니다. 저는 이 세 가지 요소를 개발 철칙으로
             사용자 경험 중심의 개발을 실천하고 있으며 동시에 팀과 함께 성장할 수 있는 건강한 개발 문화를 만들어가는 데도 관심이 많습니다.
           </p>
         </div>
